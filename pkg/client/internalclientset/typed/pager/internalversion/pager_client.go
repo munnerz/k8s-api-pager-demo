920757c0f140dbebe6248faa6880607ac1621b9b
tamal <tamal@appscode.com>
2017-09-02 06:16:09 -0700

    Generate clients properly.

--- pkg/client/internalclientset/typed/pager/internalversion/pager_client.go
+++ pkg/client/internalclientset/typed/pager/internalversion/pager_client.go
@@ -18,4 +18,82 @@ package internalversion
 
 import (
 	"github.com/munnerz/k8s-api-pager-demo/pkg/client/internalclientset/scheme"
+	rest "k8s.io/client-go/rest"
 )
+
+type PagerInterface interface {
+	RESTClient() rest.Interface
+	AlertsGetter
+}
+
+// PagerClient is used to interact with features provided by the pager group.
+type PagerClient struct {
+	restClient rest.Interface
+}
+
+func (c *PagerClient) Alerts(namespace string) AlertInterface {
+	return newAlerts(c, namespace)
+}
+
+// NewForConfig creates a new PagerClient for the given config.
+func NewForConfig(c *rest.Config) (*PagerClient, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return &PagerClient{client}, nil
+}
+
+// NewForConfigOrDie creates a new PagerClient for the given config and
+// panics if there is an error in the config.
+func NewForConfigOrDie(c *rest.Config) *PagerClient {
+	client, err := NewForConfig(c)
+	if err != nil {
+		panic(err)
+	}
+	return client
+}
+
+// New creates a new PagerClient for the given RESTClient.
+func New(c rest.Interface) *PagerClient {
+	return &PagerClient{c}
+}
+
+func setConfigDefaults(config *rest.Config) error {
+	g, err := scheme.Registry.Group("pager")
+	if err != nil {
+		return err
+	}
+
+	config.APIPath = "/apis"
+	if config.UserAgent == "" {
+		config.UserAgent = rest.DefaultKubernetesUserAgent()
+	}
+	if config.GroupVersion == nil || config.GroupVersion.Group != g.GroupVersion.Group {
+		gv := g.GroupVersion
+		config.GroupVersion = &gv
+	}
+	config.NegotiatedSerializer = scheme.Codecs
+
+	if config.QPS == 0 {
+		config.QPS = 5
+	}
+	if config.Burst == 0 {
+		config.Burst = 10
+	}
+
+	return nil
+}
+
+// RESTClient returns a RESTClient that is used to communicate
+// with API server by this client implementation.
+func (c *PagerClient) RESTClient() rest.Interface {
+	if c == nil {
+		return nil
+	}
+	return c.restClient
+}
